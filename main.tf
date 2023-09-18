3099f7f7d3dd8c934d28ef69f28fd98f1cf7c0d5
David Tausend <125006062+DavidTausend@users.noreply.github.com>
2023-09-18 14:01:23 +0200

    #13 migrate local state to terraform cloud using terraform cloud blockt (#14)

--- main.tf
+++ main.tf
@@ -1,4 +1,11 @@
 terraform {
+  cloud {
+    organization = "DavidTausend"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source  = "hashicorp/random"
