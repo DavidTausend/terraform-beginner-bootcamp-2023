f7664dbe664d5743b16130359eee99296698f35b
David Tausend <125006062+DavidTausend@users.noreply.github.com>
2023-09-18 20:33:00 +0200

    #15 generate workaround for terraform login with gitpod (#16)

---
+++ provider.tf
@@ -0,0 +1,27 @@
+terraform {
+  cloud {
+    organization = "DavidTausend"
+
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.17.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+provider "aws" {
+  # Configuration options
+}
--- resources.tf
+++ resources.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "DavidTausend"
-
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source  = "hashicorp/random"
-      version = "3.5.1"
-    }
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.17.0"
-    }
-  }
-}
-
-provider "random" {
-  # Configuration options
-}
-
-provider "aws" {
-  # Configuration options
-}
-
 # https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower   = true
