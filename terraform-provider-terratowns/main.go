a9325fd59b0081b0303c7862c66651b903618c50
DavidTausend <davidtausend-911@hotmail.com>
2023-10-02 13:14:14 +0000

    #39 starting with golang and testing the code

---
+++ terraform-provider-terratowns/main.go
@@ -0,0 +1,14 @@
+// package main: Declares the package name. 
+// The main package is special in Go, it's where the execution of the program starts.
+package main
+
+// fmt is short format, it contains functions for formatted I/O.
+import "fmt"
+
+// func main(): Defines the main function, the entry point of the app. 
+// When you run the program, it starts executing from this function.
+func main(){
+	// Format.PrintLine
+	// Prints to standard output
+	fmt.Println("Hello, world!")
+}
