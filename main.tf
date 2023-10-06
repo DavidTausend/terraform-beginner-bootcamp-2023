af0f52ae600f2ee3d308c5f3455440a68713f0a7
David Tausend <125006062+DavidTausend@users.noreply.github.com>
2023-10-06 07:48:35 -0600

    #44 test our terra home on terratowns test town (#45)

--- main.tf
+++ main.tf
@@ -18,30 +18,31 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint  = "http://localhost:4567/api"
-  user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
-  token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint  = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token     = var.terratowns_access_token
 }
-/*
+
+
 module "terrahouse_aws" {
   source              = "./modules/terrahouse_aws"
-  user_uuid           = var.user_uuid
+  user_uuid           = var.teacherseat_user_uuid
   bucket_name         = var.bucket_name
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
   content_version     = var.content_version
   assets_path         = var.assets_path
 }
-*/
+
 
 resource "terratowns_home" "german_town" {
-  name = "How survive in German"
-  description = <<DESCRIPTION
+  name            = "How survive in German"
+  description     = <<DESCRIPTION
 Moving to Germany means getting the chance to live in one of the most organized 
 and progressive countries on Earth, where you can enjoy a high standard of living.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net" //It is just a mock
-  town = "gamers-grotto"
+  domain_name     = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq3gz.cloudfront.net"
+  town            = "missingo"
   content_version = 1
 }
