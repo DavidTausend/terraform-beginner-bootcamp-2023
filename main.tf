0c9f112e27ba5f7c874bc5612f7ef66e7c468a56
David Tausend <125006062+DavidTausend@users.noreply.github.com>
2023-10-06 05:59:09 -0600

    39 terratowns provider (#43)
    * #39 starting with golang and testing the code
    
    * #39 Working to get the binary running
    
    * #39 correcting some mistake and trying again the code
    
    * #39 wip setting up the skeleton for creating a custom terraform provider
    
    * #39 trying and fixing build provider script
    
    * #39 implement terraform provider block in terraform
    
    * #39 setup skeleton for resource
    
    * #39 strating building CRUD structure
    
    * #39 resolving and looking for errors
    
    * #39 finish implimentation for terraform provider

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint  = "http://localhost:4567"
+  endpoint  = "http://localhost:4567/api"
   user_uuid = "e328f4ab-b99f-421c-84c9-4ccea042c7d1"
   token     = "9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
 }
@@ -33,3 +33,15 @@ module "terrahouse_aws" {
   assets_path         = var.assets_path
 }
 */
+
+resource "terratowns_home" "german_town" {
+  name = "How survive in German"
+  description = <<DESCRIPTION
+Moving to Germany means getting the chance to live in one of the most organized 
+and progressive countries on Earth, where you can enjoy a high standard of living.
+DESCRIPTION
+  #domain_name = module.terrahouse_aws.cloudfront_url
+  domain_name = "3fdq3gz.cloudfront.net" //It is just a mock
+  town = "gamers-grotto"
+  content_version = 1
+}
